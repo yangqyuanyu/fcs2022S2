1539a358af218934f94b9d8f9b2aa873aca0dabf
Quanyu Yang <110096950+yangqyuanyu@users.noreply.github.com>
2022-11-06 18:40:03 +1030

    Update Student.java

--- week-12/practical-exam-05/hogwarts/Student.java
+++ week-12/practical-exam-05/hogwarts/Student.java
@@ -10,41 +10,41 @@ Practical Exam Number:5
 class Student{
     private String name;
     private int age,period;
-	//Default constructor
-	Student() {
-		this.name = "unknown";
-		this.age = 0;
-		this.period = 0;
-	}
+    //Default constructor
+    Student() {
+	this.name = "unknown";
+	this.age = 0;
+	this.period = 0;
+    }
 
-	//Parametric constructor
-	Student(String tmpName, int tmpAge, int tmpPeriod) {
-		this.name = tmpName;
-		this.age = tmpAge;
-		this.period = tmpPeriod;
-	}
+    //Parametric constructor
+    Student(String tmpName, int tmpAge, int tmpPeriod){
+	this.name = tmpName;
+	this.age = tmpAge;
+	this.period = tmpPeriod;
+    }
 
-	//getter
-	public String getName(){
-		return this.name;
-	}
-	public int getAge(){
-		return this.age;
-	}
-	public int getPeriod(){
-		return this.period;
-	}
+    //getter
+    public String getName(){
+	return this.name;
+    }
+    public int getAge(){
+	return this.age;
+    }
+    public int getPeriod(){
+	return this.period;
+    }
 
-	//setter
-	public void setName(String tmpName) {
-		this.name = tmpName;
-	}
-	public void setAge(int tmpAge) {
-		this.age = tmpAge;
-	}
-	public void setPeriod(int tmpPeriod) {
-		this.period = tmpPeriod;
-	}
+    //setter
+    public void setName(String tmpName){
+	this.name = tmpName;
+    }
+    public void setAge(int tmpAge){
+	this.age = tmpAge;
+    }
+    public void setPeriod(int tmpPeriod){
+	this.period = tmpPeriod;
+    }
 
     //method printStudent
     void printStudent(){
@@ -70,5 +70,4 @@ class Student{
                 System.out.println("#"+c+" "+getName()+", "+getAge()+" years old, "+getPeriod()+"th year in Hogwarts;");
         }
     }
-
 }
