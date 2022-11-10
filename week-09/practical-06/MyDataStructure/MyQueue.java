34a563649515e114f5177f9c49f7b741e710d901
Quanyu Yang <110096950+yangqyuanyu@users.noreply.github.com>
2022-11-11 01:08:58 +1030

    Update MyQueue.java

--- week-09/practical-06/MyDataStructure/MyQueue.java
+++ week-09/practical-06/MyDataStructure/MyQueue.java
@@ -1,40 +1,40 @@
-class MyQueue {
-	private Node front; // points to the first element in queue
-	private Node rear; //  points at last element in queue
+class MyQueue{
+    private Node front; // points to the first element in queue
+    private Node rear; //  points at last element in queue
 
-	public MyQueue() {
-		this.rear = null;
-		this.front=null;
-	}
-
-
-	public void setFront(Node node) {
-        this.front = node;
+    public MyQueue(){
+	this.rear=null;
+	this.front=null;
     }
 
-    public void setRear(Node node) {
-        this.rear = node;
+
+    public void setFront(Node node){
+        this.front=node;
+    }
+
+    public void setRear(Node node){
+        this.rear=node;
     }
 
     // Getter
-    public Node getFront() {
+    public Node getFront(){
         return this.front;
     }
 
-    public Node getRear() {
+    public Node getRear(){
         return this.rear;
     }
 
-	// link element to the rear
-	public void enqueue(Node node) {
-		if(this.rear==null){
-			this.rear=node;
-			this.front=node;
-		}else{
-        	node.setNext(rear);
-        	rear = node;
-		}
+    // link element to the rear
+    public void enqueue(Node node){
+	if(this.rear==null){
+	    this.rear=node;
+	    this.front=node;
+	}else{
+            node.setNext(rear);
+            rear=node;
 	}
+    }
 
 
 	// get rid of element from the front
@@ -52,43 +52,40 @@ class MyQueue {
 
         Node tmp=this.front.getNext();
 
-		this.front.setNext(null);
-		if(tmp==null){
-		    this.front=null;
-		    this.rear=null;
-		    return null;
+	this.front.setNext(null);
+	if(tmp==null){
+	    this.front=null;
+	    this.rear=null;
+	    return null;
         }else{
             return tmp.getData();
         }
     }
 
-	public boolean isEmpty(){
-		return rear == null && front == null;
-	}
+    public boolean isEmpty(){
+	return rear==null&&front==null;
+    }
 
 
-	public void displayQueue() {
-        if (isEmpty()) {
+    public void displayQueue(){
+        if(isEmpty()){
             System.out.println("Queue is empty");
             return;
         }
-
-
-        Node temp = front;      //temp pointer
-        while (temp != null) {
+        Node temp=front;      //temp pointer
+        while(temp!=null){
             temp.printNode();
-            temp = temp.getNext();
+            temp=temp.getNext();
         }
-
     }
 
     public Node peek(){
-		if (this.front == null||rear==null){
-			System.out.println("Queue is empty");
-			return null;
-		} else {
-		    return front;
-		}
+	if (this.front==null||rear==null){
+	    System.out.println("Queue is empty");
+	    return null;
+	}else{
+	    return front;
 	}
+    }
 
 }
