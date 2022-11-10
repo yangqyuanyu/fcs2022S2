439786587682aa3608af0774b75fae75c84148c5
Quanyu Yang <110096950+yangqyuanyu@users.noreply.github.com>
2022-11-11 02:37:22 +1030

    Update WaitingList.java

--- week-09/practical-06/EmergencyServiceSystem/WaitingList.java
+++ week-09/practical-06/EmergencyServiceSystem/WaitingList.java
@@ -22,66 +22,42 @@ public class WaitingList{
             return max.patient;
         }
         else if (this.head!=null){
-            if (this.head.next!=null) {
-
+            if (this.head.next!=null){
                 Node temp = this.head;
                 Node prev = null;
-                while(temp.next !=null) {
-
-                    if (temp.patient.getTriageLevel() == max.patient.getTriageLevel()) {
-
+                while(temp.next !=null){
+                    if(temp.patient.getTriageLevel() == max.patient.getTriageLevel()){
                         prev.next = temp.next;
                         return temp.patient;
-
-                    }else {
-
+                    }else{
                         prev = temp;
                         temp = temp.next;
                     }
                 }
-            }
-
-            else {
-
+            }else{
                 return head.patient;
-
             }
-
         }
-
         return null;
-
     }
 
     //Defining private node.
     private Node maxNode() {
-
         Node max = this.head;
         Node temp = this.head;
-
         if (this.head == null) {
-
             System.out.println("Error! The waiting list is empty.");
-
             return null;
         }
-
-        while(temp.next!=null) {
-
+        while(temp.next!=null){
             if (temp.patient.getTriageLevel()>max.patient.getTriageLevel()) {
-
                 max = temp;
                 temp = temp.next;
-
-            } else {
-
+            }else{
                 temp = temp.next;
-
             }
         }
-
         return max;
-
     }
 
     /**
@@ -91,12 +67,10 @@ public class WaitingList{
 
     //Defining method - addToList
     public void addToList(Patient patient) {
-
         Node node = new Node(patient);
         Node temp = this.head;
         this.head = node;
         node.setNext(temp);
-
     }
 
     //Defining method for printing details.
@@ -113,92 +87,65 @@ public class WaitingList{
 
 
     //Check whether the patient is in this list or not
-    public boolean isInList(Patient patient) {
-
+    public boolean isInList(Patient patient){
         if (this.head == null) {
             return false;
         }
-
         Node temp = this.head;
-
-        while(temp != null) {
-
+        while(temp!=null) {
             if(temp.getPatient().getName().equals(patient.getName())
                     && temp.getPatient().getPhoneNumber().equals(patient.getPhoneNumber())){
                 return true;
             }
-
             temp = temp.getNext();
         }
-
         return false;
-
     }
 
     //Defining remove method
-    public void remove(int id) {
-
-        if(this.head != null) {
-
-            Node temp = this.head;
-            Node prev = null;
-
-            do {
-
-                if (temp.patient.getId() == id) {
-
-                    if(prev != null) {
-
-                        prev.next = temp.next;
-                        temp = temp.next;
-
-                    } else {
-
-                        this.head = this.head.next;
-                        prev = null;
-                        temp = this.head;
+    public void remove(int id){
+        if(this.head!=null){
+            Node temp=this.head;
+            Node prev=null;
+            do{
+                if(temp.patient.getId()==id){
+                    if(prev!=null){
+                        prev.next=temp.next;
+                        temp=temp.next;
+                    }else{
+                        this.head=this.head.next;
+                        prev=null;
+                        temp=this.head;
                     }
-
-                } else {
-
-                    prev = temp;
-                    temp = temp.next;
-
+                }else{
+                    prev=temp;
+                    temp=temp.next;
                 }
-
-            } while(temp != null);
+            }while(temp!=null);
         }
     }
 
-    //DEfining method getNumberPatient
+    //Defining method getNumberPatient
     public int getNumberPatients(int id) {
-        int counter = 0;
-
-        if (this.head !=null) {
-
-            Node temp = this.head;
-
-            do {
-
-                if (temp.patient.getId()==id) {
+        int counter=0;
+        if (this.head!=null){
+            Node temp=this.head;
+            do{
+                if(temp.patient.getId()==id){
                     break;
                 }
-
-                temp = temp.next;
-
-            } while(temp!=null);
+                temp=temp.next;
+            }while(temp!=null);
 
             Node node = temp;
             temp = this.head;
 
-            do {
-
-                if (node.patient.getTriageLevel()<temp.patient.getTriageLevel()) {
+            do{
+                if(node.patient.getTriageLevel()<temp.patient.getTriageLevel()){
                     counter++;
                 }
-
-                temp = temp.next;
-            } while(temp!=null);
+                temp=temp.next;
+            }while(temp!=null);
         }
         return counter;
     }
