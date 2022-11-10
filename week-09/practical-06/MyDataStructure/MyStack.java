34661b85df714232d010970a13321fcb5de595fc
Quanyu Yang <110096950+yangqyuanyu@users.noreply.github.com>
2022-11-11 01:13:12 +1030

    Update MyStack.java

--- week-09/practical-06/MyDataStructure/MyStack.java
+++ week-09/practical-06/MyDataStructure/MyStack.java
@@ -5,40 +5,35 @@ public class MyStack extends Node{
         this.top=null;
     }
 
-
 //To push a Node on top of the Stack, first we must create a new node Node node【 = new Node(data)】
 //Then this new node will become the top. This node will then point to the old top value(node.next = top) and then the top points to the node (top = node).
-    public void push(Node node) {
+    public void push(Node node){
         	node.setNext(this.top);
         	this.top = node;
+    }
+
+    public Node pop(){
+        if (this.top == null){
+	    System.out.println("Stack is empty");
+	    return null;
+	}else{
+	    Node temp=this.top;
+	    this.top = this.top.getNext();
+	    return temp;
 	}
+    }
 
-
-
-	public Node pop(){
-	    if (this.top == null){
-			System.out.println("Stack is empty");
-			return null;
-		} else {
-			Node temp=this.top;
-			this.top = this.top.getNext();
-			return temp;
-		}
+    public Node peek(){
+	if(this.top==null){
+	    System.out.println("Stack is empty");
+	    return null;
+	}else{
+            return top;
 	}
-
-	public Node peek(){
-		if (this.top == null){
-			System.out.println("Stack is empty");
-			return null;
-		} else {
-		    return top;
-		}
-	}
+    }
 
 
-	public boolean isEmpty(){
-		return (this.top==null);
-	}
-
-
+    public boolean isEmpty(){
+	return (this.top==null);
+    }
 }
